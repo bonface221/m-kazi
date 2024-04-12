9eb8a872bfeec5fdaa64c8c5330025dae574657e
bonface221 <mainaboniface221@gmail.com>
2024-04-12 12:52:22 +0300

    add redis url

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import menu from "./ussd";
 import Redis from "ioredis";
 
 require("dotenv").config();
+const REDIS_URL = process.env.REDIS_URL!;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-export const redis = new Redis(6379, "redis://127.0.0.1:6379/0");
+export const redis = new Redis(6379, REDIS_URL);
 
 const port = process.env.PORT || 3000;
 
